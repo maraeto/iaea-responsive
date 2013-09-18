6c540049a3f3e96893622f92267ade30e1024cd3
Kresimir Nikolic <kresimir.nikolic@gmail.com>
2013-09-18 19:17:51 +0200

    Remove "Welcome to…" from the Front Page
    Signed-off-by: Kresimir Nikolic <kresimir.nikolic@gmail.com>

--- sites/all/themes/IAEA/template.php
+++ sites/all/themes/IAEA/template.php
@@ -3,3 +3,11 @@
 /**
  * @file template.php
  */
+
+/** 
+* Remove "Welcome to ... "  from the front page. This message is rendered when no content is available,
+* e.g. when content is rendered via Context or Views module
+*/
+function IAEA_preprocess_page(&$variables) {
+	if (drupal_is_front_page()) { $variables['title']=""; }
+}
