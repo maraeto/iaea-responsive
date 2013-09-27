0d4353d8320ab534591b55b79dbd56c640c01a28
Kresimir Nikolic <kresimir.nikolic@gmail.com>
2013-09-27 16:04:08 +0200

    Content added
    Signed-off-by: Kresimir Nikolic <kresimir.nikolic@gmail.com>

--- sites/all/themes/IAEA/template.php
+++ sites/all/themes/IAEA/template.php
@@ -11,3 +11,13 @@
 function IAEA_preprocess_page(&$variables) {
 	if (drupal_is_front_page()) { $variables['title']=""; }
 }
+
+/* function IAEA_preprocess_node(&$vars) {
+  if ($vars['type'] === 'news_story') {
+    $date = current($vars['content']['field_date']['#items']);
+    $date_ts = strtotime($date['value']);
+    $vars['content']['field_date'][0] = array(
+      '#markup' => '<strong>' . date('d', $date_ts) . '</strong> <em>' . date('F Y', $date_ts) . '</em>',
+    );    
+  }
+}*/
