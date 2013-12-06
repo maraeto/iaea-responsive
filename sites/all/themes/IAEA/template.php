69ce691b94877813080b96c1ed1770cd8586234a
Kresimir Nikolic <kresimir.nikolic@gmail.com>
2013-12-06 14:37:16 +0100

    menu_tree Theme Function extended: added menu options "divider" and "dropdown-header"
    To add divider: add link in main menu, 	call it DIVIDER, insert "http://#divider" in path field, and drag the divider to desired position.
    
    To add heading: add link in main menu, call it HEADING name, insert "http://#dropdown-header" in path field, add the name to the description field, and drag the heading to desired position.
    
    Signed-off-by: Kresimir Nikolic <kresimir.nikolic@gmail.com>

--- sites/all/themes/IAEA/template.php
+++ sites/all/themes/IAEA/template.php
@@ -73,3 +73,59 @@ function IAEA_preprocess_field(&$variables) {
 function IAEA_menu_tree(&$variables) {
   return '<ul class="menu nav nav-pills nav-stacked">' . $variables['tree'] . '</ul>';
 }
+
+
+/* Adding option to include divider in menu (bootstrap) <li class="divider"></li>  */
+function IAEA_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    else {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+
+      // Check if this element is nested within another.
+      if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
+        // Generate as dropdown submenu.
+        $element['#attributes']['class'][] = 'dropdown-submenu';
+      }
+      else {
+        // Generate as standard dropdown.
+        $element['#attributes']['class'][] = 'dropdown';
+        $element['#localized_options']['html'] = TRUE;
+        $element['#title'] .= ' <span class="caret"></span>';
+      }
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading
+      // when a submenu link is clicked.
+      $element['#localized_options']['attributes']['data-target'] = '#';
+    }
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+
+  if (strpos($element['#href'], '#divider')) {
+    $return = '<li class="divider"></li>';
+  }
+  else if (strpos($element['#href'], '#dropdown-header')) {
+    $return = '<li class="dropdown-header">' . $element['#localized_options']['attributes']['title'] . '</li>';
+  }
+  else {
+    $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+    $return = '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+  }
+
+  return $return;
+}
