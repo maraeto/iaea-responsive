2dbf38a2b2e33aaecc58e5622de8b426f92b3c36
Kresimir Nikolic <kresimir.nikolic@gmail.com>
2014-01-13 10:35:31 +0100

    Bootstrap_modal module update: 7.x-.3.0 to 7.x-.3.3
    Signed-off-by: Kresimir Nikolic <kresimir.nikolic@gmail.com>

--- sites/all/modules/twitter_bootstrap_modal/twitter_bootstrap_modal.js
+++ sites/all/modules/twitter_bootstrap_modal/twitter_bootstrap_modal.js
@@ -1,36 +1,44 @@
+(function ($) {
+
 /**
 * @file
 * Javascript support files.
 *
 */
 
-jQuery(document).ready(function() {
-  // Actions to make link Twitter Bootstrap Modal
-  var TBtrigger = Drupal.settings.jquery_ajax_load.TBtrigger;
-  var i = 0;
-  // Puede ser más de un valor, hay que usar foreach()
-  jQuery(TBtrigger).each(function() {
-    var html_string = jQuery(this).attr( 'href' );
-    // Hay que validar si la ruta trae la URL del sitio
-    jQuery(this).attr( 'href' , '/jquery_ajax_load/get' + html_string );
-    jQuery(this).attr( 'data-target' , '#jquery_ajax_load' + i );
-    jQuery(this).attr( 'data-toggle' , 'modal' );
-    twitter_bootstrap_modal_create_modal(i++);
-  });
-  jQuery('.bs_modal').each(function() {
-    var html_string = jQuery(this).attr( 'href' );
-    // Hay que validar si la ruta trae la URL del sitio
-    jQuery(this).attr( 'href' , '/bs_modal' + html_string );
-  });
-});
+Drupal.behaviors.twitter_bootstrap_modal = {
+  attach: function (context, settings) {
+    // Actions to make link Twitter Bootstrap Modal
+    var TBtrigger = Drupal.settings.jquery_ajax_load.TBtrigger;
+    var i = 0;
+    // Puede ser más de un valor, hay que usar foreach()
+    $(TBtrigger).once(function() {
+      var html_string = $(this).attr( 'href' );
+      // Hay que validar si la ruta trae la URL del sitio
+      $(this).attr( 'href' , '/jquery_ajax_load/get' + html_string );
+      $(this).attr( 'data-target' , '#jquery_ajax_load' + i );
+      $(this).attr( 'data-toggle' , 'modal' );
+      twitter_bootstrap_modal_create_modal(i++);
+    });
+    var TBmodaltrigger = Drupal.settings.jquery_ajax_load.TBmodaltrigger;
+    $(TBmodaltrigger).once(function() {
+      var html_string = $(this).attr( 'href' );
+      // Hay que validar si la ruta trae la URL del sitio
+      $(this).attr( 'href' , '/bs_modal' + html_string );
+    });
+  }
+}
 
 function twitter_bootstrap_modal_create_modal(i) {
   var modal_name = Drupal.settings.jquery_ajax_load.TBname;
   var modal_module = Drupal.settings.jquery_ajax_load.TBmodule;
-  jQuery('body').append('<div id="jquery_ajax_load' + i + '" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">');
-  jQuery('#jquery_ajax_load' + i).append('<div class="modal-dialog" />');
-  jQuery('#jquery_ajax_load' + i + ' .modal-dialog').append('<div class="modal-content" />');
-  jQuery('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 class="modal_title">' + modal_name + '</h3></div>');
-  jQuery('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-body"><span class="text-warning">' + Drupal.t('Loading') + '... </span><img src="' + modal_module + '/twitter_bootstrap_modal_loading.gif"></div>');
-  jQuery('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">' + Drupal.t('Close') + '</button></div>');
+  $('body').append('<div id="jquery_ajax_load' + i + '" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">');
+  $('#jquery_ajax_load' + i).append('<div class="modal-dialog" />');
+  $('#jquery_ajax_load' + i + ' .modal-dialog').append('<div class="modal-content" />');
+  $('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 class="modal_title">' + modal_name + '</h3></div>');
+  $('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-body"><span class="text-warning">' + Drupal.t('Loading') + '... </span><img src="/' + modal_module + '/twitter_bootstrap_modal_loading.gif"></div>');
+  $('#jquery_ajax_load' + i + ' .modal-content').append('<div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">' + Drupal.t('Close') + '</button></div>');
 }
+
+}(jQuery));
+
