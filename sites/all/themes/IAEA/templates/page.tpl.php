7aab817ed5a0e85ddf461f89e065b0f218909e06
Kresimir Nikolic <kresimir.nikolic@gmail.com>
2014-01-13 09:01:41 +0100

    Drop down on hover: sidebar navigation added, so the same approach works for secondary navigation
    Signed-off-by: Kresimir Nikolic <kresimir.nikolic@gmail.com>

--- sites/all/themes/IAEA/templates/page.tpl.php
+++ sites/all/themes/IAEA/templates/page.tpl.php
@@ -131,13 +131,13 @@
 <script>
 /* drop down menu on hover */
   jQuery(document).ready(function(){
-    jQuery('ul.nav li.dropdown').hover(function() {
+    jQuery('ul.nav li.dropdown, ul.nav li.dropdown-submenu').hover(function() {
       jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
     }, function() {
       jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
     });
-/* follow the link on click */
-    jQuery('ul.nav li.dropdown').click( function(e) {
+/* follow the link on */
+    jQuery('ul.nav li.dropdown, ul.nav li.dropdown-submenu').click( function(e) {
       e.stopPropagation();
 } );
 });
